5f2d2d05fe2ccbe39adbbaccf1f2c1df520f4dce
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-07-27 22:36:55 +0100

    open doors

--- client.lua
+++ client.lua
@@ -422,3 +422,12 @@ function Openbank(bankName)
             ClearPedTasks(PlayerPedId())
         end)
 end
+-- open doors
+CreateThread(function()
+    for door, state in pairs(Config.Doors) do
+        if not IsDoorRegisteredWithSystem(door) then
+            Citizen.InvokeNative(0xD99229FE93B46286, door, 1, 1, 0, 0, 0, 0)
+        end
+        DoorSystemSetDoorState(door, state)
+    end
+end)
