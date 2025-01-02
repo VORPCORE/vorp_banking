e836b089e0eff8ded3be0128f7c9e8db347a73c8
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-01-02 19:00:15 +0000

    version 1.7

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
-fx_version "adamant"
+fx_version "cerulean"
 game "rdr3"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'VORP edit by RobiZona#0001'
+
 description 'Bank system VORP'
 lua54 'yes'
 
@@ -21,13 +22,8 @@ server_scripts {
     'server/server.lua',
 }
 
-dependencies {
-    'vorp_core',
-    'vorp_inventory'
-}
-
 --dont touch
-version '1.6'
+version '1.7'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_banking'
